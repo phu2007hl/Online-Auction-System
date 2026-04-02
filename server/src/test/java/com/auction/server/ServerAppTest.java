8d404f5d52d1e92fa7cd4750b91c8524de33de8d
Đặng Đức Phú <25020316@vnu.edu.vn>
2026-04-02 12:21:49 +0700

    chore: init Maven multi-module structure with IntelliJ

---
+++ server/src/test/java/com/auction/server/ServerAppTest.java
@@ -0,0 +1,11 @@
+package com.auction.server;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ServerAppTest {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
