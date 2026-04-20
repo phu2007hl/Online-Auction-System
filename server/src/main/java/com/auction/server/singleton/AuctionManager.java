41a1b41e063a6e91b273777f267aa2a205737b55
Đặng Đức Phú <25020316@vnu.edu.vn>
2026-04-20 22:07:11 +0700

    Triển khai thread-safe cho Singleton

--- server/src/main/java/com/auction/server/singleton/AuctionManager.java
+++ server/src/main/java/com/auction/server/singleton/AuctionManager.java
@@ -1,11 +1,15 @@
 package com.auction.server.singleton;
 
 public class AuctionManager{
-    private static AuctionManager instance;
+    private static volatile AuctionManager instance;
     private AuctionManager(){}
     public static AuctionManager getInstance() {
         if (instance == null) {
-            instance = new AuctionManager();
+            synchronized (AuctionManager.class){
+                if (instance == null) {
+                    instance = new AuctionManager();
+                }
+            }
         }
         return instance;
     }
