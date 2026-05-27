dd084fdc63fcdb54ee42f148d3c4ff788aa0de52
FongDangT <25020309@uet.edu.vn>
2026-05-27 13:53:02 +0700

    Thêm thời gian cho lịch sử bid

--- sharemodel/src/main/java/com/auction/shared/auction/BidTransaction.java
+++ sharemodel/src/main/java/com/auction/shared/auction/BidTransaction.java
@@ -1,16 +1,23 @@
 package com.auction.shared.auction;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class BidTransaction implements Serializable {
   private static final long serialVersionUID = 1L;
 
   private String bidderUsername;
   private double bidAmount;
+  private LocalDateTime bidTime;
 
   public BidTransaction(String bidderUsername, double bidAmount) {
+    this(bidderUsername, bidAmount, LocalDateTime.now());
+  }
+
+  public BidTransaction(String bidderUsername, double bidAmount, LocalDateTime bidTime) {
     this.bidderUsername = bidderUsername;
     this.bidAmount = bidAmount;
+    this.bidTime = bidTime;
   }
 
   public String getBidderUsername() {
@@ -21,4 +28,7 @@ public class BidTransaction implements Serializable {
     return bidAmount;
   }
 
+  public LocalDateTime getBidTime() {
+    return bidTime;
+  }
 }
